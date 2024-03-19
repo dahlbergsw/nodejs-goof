1afdeec8be48b99ca9bdba05bc67ff3c63afad31
samuel.dahlberg <dahlbergsw@gmail.com>
2024-03-19 19:54:35 -0400

    Add initial workflow job to call create issue with dummy data

---
+++ create_github_issue.py
@@ -0,0 +1,44 @@
+import requests
+import json
+
+GITHUB_TOKEN = os.getenv('PERSONAL_ACCESS_TOKEN')
+GITHUB_REPOSITORY = os.getenv('GITHUB_REPOSITORY')
+
+def parse_sarif_file(sarif_file_path):
+    pass
+
+def create_github_issue():
+    # TODO: Remove
+    title = "Test"
+    body = "Test"
+
+    # END_TODO
+    
+    url = f'https://api.github.com/repos/{GITHUB_REPOSITORY}/issues'
+    print(url)
+
+    headers = {
+        "Authorization": f"Bearer {GITHUB_TOKEN}",
+        "Accept": "application/vnd.github+json"
+    }
+
+    payload = {
+        'title': title,
+        'body': body
+    }
+    
+    data = json.dumps(payload)
+
+    try:
+        response = requests.post(url, data=data, headers=headers)
+    except Exeption as e:
+        print(e)
+
+    if response.status_code < 300:
+        print(f'Successfully created issue: {title}, Status Code: {response.status_code}')
+    else:
+        print(f'Failed to create issue: {title}, Status Code: {response.status_code}')
+        
+if __name__=='__main__':
+    create_github_issue()
+    
