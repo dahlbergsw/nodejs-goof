359f2d63f64657b07ed229d858788982cc2baed1
samuel.dahlberg <dahlbergsw@gmail.com>
2024-03-19 21:12:10 -0400

    Add test code for reading in sarif file generated by Snyk

--- create_github_issue.py
+++ create_github_issue.py
@@ -4,9 +4,39 @@ import os
 
 GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
 GITHUB_REPOSITORY = os.getenv('GITHUB_REPOSITORY')
+SARIF_FILE = 0s.getenv('SARIF_FILE')
 
 def parse_sarif_file(sarif_file_path):
+
+    with open(sarif_file_path, 'r') as f:
+        print(f.readlines())
+    
     pass
+
+def read_critical_vulnerabilities_from_scanner(page: int, per_page: int = 100):
+    url = f"https://api.github.com/repos/{GITHUB_REPOSITORY}/code-scanning/alerts"
+
+    headers = {
+        "Authorization": f"Bearer {GITHUB_TOKEN}",
+        "Accept": "application/vnd.github+json"
+    }
+
+    params = {
+        "state": "critical",
+        "page": page,
+        "per_page": per_page
+        }
+
+    try:
+        response = requests.get(url, params=params, headers=headers)
+    except Exception as e:
+        print(e)
+
+    if response.status_code < 300:
+        print(f'Successfully retrieved Code Scanning results, Status Code: {response.status_code}')
+    else:
+        print(f'Failed to retriev Code Scanning results, Status Code: {response.status_code}')        
+
 
 def create_github_issue():
     # TODO: Remove
@@ -32,7 +62,7 @@ def create_github_issue():
 
     try:
         response = requests.post(url, data=data, headers=headers)
-    except Exeption as e:
+    except Exception as e:
         print(e)
 
     if response.status_code < 300:
@@ -41,5 +71,7 @@ def create_github_issue():
         print(f'Failed to create issue: {title}, Status Code: {response.status_code}')
         
 if __name__=='__main__':
-    create_github_issue()
+    parse_sarif_file(SARIF_FILE)
+    # read_critical_vulnerabilities_from_scanner(page=1)
+    # create_github_issue()
     
