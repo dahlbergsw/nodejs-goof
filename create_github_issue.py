cb8b4598b42a465ca928a7e5e16b8b26f94105b0
samuel.dahlberg <dahlbergsw@gmail.com>
2024-03-20 00:27:11 -0400

    Add parsing and formatting for github issue

--- create_github_issue.py
+++ create_github_issue.py
@@ -1,48 +1,45 @@
 import requests
 import json
 import os
+from json2table import convert
 
 GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
 GITHUB_REPOSITORY = os.getenv('GITHUB_REPOSITORY')
-VULN_SCAN_FILE = os.getenv('VULN_SCAN_FILE')
 
-def parse_sarif_file(file_path):
+def parse_json_file(file_path):
     with open(file_path, 'r') as f:
-        print(f.readlines())
+        data = json.load(f)
+        vulnerabilities = data["vulnerabilities"]
+
+        vuln_descriptions = []
+        
+        for vuln in vulnerabilities:
+            line = {
+                "id": vuln["id"],
+                "title": vuln["title"],
+                "package": vuln["packageName"],
+                "version": vuln["version"]
+                }
+
+            vuln_descriptions.append(line)
+
+        return vuln_descriptions
+
+def format_vulns(vulns):
+    html = "<table>"
+    html += "<tr><th>ID</th><th>Title</th><th>Package</th><th>Package Version</th>"
+
+    for vuln in vulns:
+        html += "<tr><td>" + vuln["id"] + "</th><td>" + vuln["title"] + "</th><td>" + vuln["package"] + "</td><td>" + vuln["version"] + "</th>"
+
+    html += "</table>"
+
+    return html
+        
     
-    pass
+def create_github_issue(vulns: str):
 
-def read_critical_vulnerabilities_from_scanner(page: int, per_page: int = 100):
-    url = f"https://api.github.com/repos/{GITHUB_REPOSITORY}/code-scanning/alerts"
-
-    headers = {
-        "Authorization": f"Bearer {GITHUB_TOKEN}",
-        "Accept": "application/vnd.github+json"
-    }
-
-    params = {
-        "state": "critical",
-        "page": page,
-        "per_page": per_page
-        }
-
-    try:
-        response = requests.get(url, params=params, headers=headers)
-    except Exception as e:
-        print(e)
-
-    if response.status_code < 300:
-        print(f'Successfully retrieved Code Scanning results, Status Code: {response.status_code}')
-    else:
-        print(f'Failed to retriev Code Scanning results, Status Code: {response.status_code}')        
-
-
-def create_github_issue():
-    # TODO: Remove
-    title = "Test"
-    body = "Test"
-
-    # END_TODO
+    title = "Critical Vulnerabilities found for PR: "
     
     url = f'https://api.github.com/repos/{GITHUB_REPOSITORY}/issues'
     print(url)
@@ -51,6 +48,8 @@ def create_github_issue():
         "Authorization": f"Bearer {GITHUB_TOKEN}",
         "Accept": "application/vnd.github+json"
     }
+
+    body = format_vulns(vulns)
 
     payload = {
         'title': title,
@@ -70,11 +69,6 @@ def create_github_issue():
         print(f'Failed to create issue: {title}, Status Code: {response.status_code}')
         
 if __name__=='__main__':
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    print(f"dir_path: {dir_path}")
-    print(os.listdir(dir_path))
-    print(f"Vuln Scan File: {VULN_SCAN_FILE}")
-    parse_sarif_file("critical_vuln_scan.json")
-    # read_critical_vulnerabilities_from_scanner(page=1)
-    # create_github_issue()
+    vulns = parse_json_file("critical_vuln_scan.json")
+    create_github_issue(vulns)
     
